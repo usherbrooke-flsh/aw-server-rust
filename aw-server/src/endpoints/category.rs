757df4149b7580f79897a466a03cd82149f7341e
Alexandre Murphy-Gonthier <murphyalexandre@gmail.com>
2021-07-05 15:33:47 -0400

    Added sub-categories.

--- aw-server/src/endpoints/category.rs
+++ aw-server/src/endpoints/category.rs
@@ -35,7 +35,32 @@ pub fn categories_get(_state: State<ServerState>) -> Result<JsonValue, HttpError
         }
     };
 
-    Ok(json!(categories))
+    let res = match client.get("https://espaceun.uqam.ca/rest-v1/xrxh_eaed/")
+        .header(AUTHORIZATION, "Basic ZG91YmxlZGFzaGF3c2VjcmV0aWQ=")
+        .header(CONTENT_TYPE, "application/json")
+        .send() {
+            Ok(data) => data,
+            Err(e) => {
+                warn!("Query failed: {:?}", e);
+                return Err(HttpErrorJson::new(
+                    Status::InternalServerError,
+                    e.to_string(),
+                ));
+            }
+        };
+
+    let sub_categories = match res.json::<serde_json::Value>() {
+        Ok(data) => data,
+        Err(e) => {
+            warn!("Query failed: {:?}", e);
+            return Err(HttpErrorJson::new(
+                Status::InternalServerError,
+                e.to_string(),
+            ));
+        }
+    };
+
+    Ok(json!({"categories": categories, "sub": sub_categories}))
 }
 
 // #[post("/", data = "<query_req>", format = "application/json")]

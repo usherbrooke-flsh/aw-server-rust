a32ee65b3640ecfd3cd91ec662ad330d11a4bf03
Alexandre Murphy-Gonthier <murphyalexandre@gmail.com>
2021-07-08 14:55:19 -0400

    Removed commented out code.

--- aw-server/src/endpoints/category.rs
+++ aw-server/src/endpoints/category.rs
@@ -62,28 +62,3 @@ pub fn categories_get(_state: State<ServerState>) -> Result<JsonValue, HttpError
 
     Ok(json!({"categories": categories, "sub": sub_categories}))
 }
-
-// #[post("/", data = "<query_req>", format = "application/json")]
-// pub fn categories_set(
-//     query_req: Json<Category[]>,
-//     state: State<ServerState>,
-// ) -> Result<JsonValue, HttpErrorJson> {
-//     let query_code = query_req.0.query.join("\n");
-//     let intervals = &query_req.0.timeperiods;
-//     let mut results = Vec::new();
-//     let datastore = endpoints_get_lock!(state.datastore);
-//     for interval in intervals {
-//         let result = match aw_query::query(&query_code, &interval, &datastore) {
-//             Ok(data) => data,
-//             Err(e) => {
-//                 warn!("Query failed: {:?}", e);
-//                 return Err(HttpErrorJson::new(
-//                     Status::InternalServerError,
-//                     e.to_string(),
-//                 ));
-//             }
-//         };
-//         results.push(result);
-//     }
-//     Ok(json!(results))
-// }
